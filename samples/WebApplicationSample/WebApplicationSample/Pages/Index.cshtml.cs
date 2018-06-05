fae0d2423f8fdbf4c9421923a7665402835f3771
Dan Friedman <dan@fantasticfriedmans.com>
2018-06-05 04:30:07 -0500

    Add sample

---
+++ samples/WebApplicationSample/WebApplicationSample/Pages/Index.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+
+namespace WebApplicationSample.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly IConfiguration _configuration;
+
+        public IndexModel(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string AllowedHosts { get; set; }
+        public string PackageName { get; set; }
+        public string RepoType { get; set; }
+
+        public void OnGet()
+        {
+            AllowedHosts = _configuration["AllowedHosts"];
+            PackageName = _configuration["name"];
+            RepoType = _configuration["Repository:Type"];   // case insensitive
+        }
+    }
+}
---
+++ samples/WebApplicationSample/WebApplicationSample/Program.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using YellowCounter.FileProvider.Url;
+
+namespace WebApplicationSample
+{
+    public class Program
+    {
+        private const string ConfigUrl = "https://raw.githubusercontent.com/cdnjs/cdnjs/master/ajax/libs/aurelia/package.json";
+
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+            // Use `UrlFileProvider` to read a file on the web
+            .ConfigureAppConfiguration((c, b) => b.AddJsonFile(
+                new UrlFileProvider(),
+                ConfigUrl,
+                optional: false,
+                reloadOnChange: false))
+            .UseStartup<Startup>();
+    }
+}
